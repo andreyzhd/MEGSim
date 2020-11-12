55f2f64a2fae9cb3a4e3756deeeede373e00016c
Andrey Zhdanov <andrey.zhdanov@alumni.aalto.fi>
2020-11-12 16:47:41 +0200

    Minor improvement

--- scripts/exp10_iter_opt_vis.py
+++ scripts/exp10_iter_opt_vis.py
@@ -15,10 +15,14 @@ fl = open(DATA_FNAME, 'rb')
 x_helm, y_helm, z_helm, sens_indx, best_cond_nums = pickle.load(fl)
 fl.close()
 
+#%% Print some info
+print('%i sensors placed over %s possible locations' % (len(sens_indx), len(x_helm)))
+print('The final condition number is %f' % best_cond_nums[-1])
+
 #%% Plot the helmet
 fig1 = mlab.figure(1, bgcolor=(1, 1, 1), fgcolor=(0, 0, 0))
 mlab.clf(fig1)
-mlab.points3d(x_helm, y_helm, z_helm, scale_factor=0.01, color=(0, 0, 0))
+mlab.points3d(x_helm, y_helm, z_helm, scale_factor=0.02, color=(0, 0, 0))
 mlab.points3d(x_helm[sens_indx], y_helm[sens_indx], z_helm[sens_indx], scale_factor=0.02, color=(0, 1, 0))
 
 #%% Look at the condition numbers
@@ -26,7 +30,3 @@ plt.plot(np.log10(best_cond_nums))
 plt.xlabel('Number of sensors')
 plt.ylabel('Log10 of condition number')
 plt.show()
-
-print('The final condition number is %f' % best_cond_nums[-1])
-
-input("Press Enter to continue...")
