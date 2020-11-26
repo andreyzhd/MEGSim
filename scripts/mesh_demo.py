1e824464b7aaaf1be11faf38d03dfd4aef6aa307
Jussi Nurminen <jnu@iki.fi>
2020-11-26 11:57:58 +0200

    equidistant pts demo

--- scripts/mesh_demo.py
+++ scripts/mesh_demo.py
@@ -13,8 +13,8 @@ import matplotlib.pylab as plt
 import trimesh
 from IPython import get_ipython
 
-from megsimutils.mesh import _delaunay_tri, _my_tri
-from megsimutils.viz import _mlab_points3d, _mlab_trimesh
+from megsimutils.mesh import _delaunay_tri, _my_tri, trimesh_equidistant_vertices
+from megsimutils.viz import _mlab_points3d, _mlab_quiver3d, _mlab_trimesh
 from megsimutils.array_geometry import barbute, spherical
 
 
@@ -36,13 +36,41 @@ Sc, Sn = barbute(
     NSENSORS_UPPER, NSENSORS_LOWER, ARRAY_RADIUS, HEIGHT_LOWER, PHISPAN_LOWER
 )
 pts = Sc.copy()
+
+# unfortunately, delaunay doesn't do a great job here, since the projection to
+# 2D plane is a bit pathological
 tris = _delaunay_tri(pts)
 fig = mlab.figure()
 _mlab_trimesh(pts, tris, figure=fig, representation='wireframe')
+mlab.title('triangulated')
 
+# supersample mesh to 5 mm resolution
 fig = mlab.figure()
 pts_, tris_ = trimesh.remesh.subdivide_to_size(pts, tris, 5e-3)
 _mlab_trimesh(pts_, tris_, figure=fig, representation='wireframe')
+mlab.title('supersampled')
+
+# make a trimesh object and clean it a bit
+tm = trimesh.Trimesh(vertices=pts_, faces=tris_)
+trimesh.smoothing.filter_humphrey(tm)  # not sure if necessary
+trimesh.repair.fix_winding(tm)
+trimesh.repair.fix_inversion(tm)
+trimesh.repair.fill_holes(tm)
+# reobtain the points and vertices
+pts_, tris_ = tm.vertices, tm.faces
+
+# find the equidistant vertices
+sensor_verts = trimesh_equidistant_vertices(tm, 500)
+
+# convert trimesh TrackedArray -> np.array for speed
+Sc = np.array(pts_[sensor_verts, :])
+Sn = tm.vertex_normals[sensor_verts, :]
+
+# voila (?)
+fig = mlab.figure()
+_mlab_trimesh(pts_, tris_, figure=fig)
+_mlab_points3d(Sc, figure=fig, scale_factor=.006)
+mlab.title('resulting array')
 
 
 
