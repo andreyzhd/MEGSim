ec796803bf7054a325d9e2cabebad33b6374c889
Andrey Zhdanov <andrey.zhdanov@alumni.aalto.fi>
2020-12-17 14:02:41 +0200

    minor changes

--- scripts/visualize_vsh_vect.py
+++ scripts/visualize_vsh_vect.py
@@ -1,3 +1,6 @@
+"""
+3D visualizations of some VSH components
+"""
 import numpy as np
 from mayavi import mlab
 
@@ -9,15 +12,24 @@ N_POINTS = 10000
 LIN = 16
 LOUT = 0
 
-DEG = 13
-ORDER = 9
+DEG_1 = 16
+ORDER_1 = 11
+DEG_2 = 2
+ORDER_2 = 0
+
 CMAP = 'cool'
+USE_SPHERE = True
 
 # Generate the surface and the normals
-rhelm = spherepts_golden(N_POINTS, hcylind=1)
+if USE_SPHERE:
+    rhelm = spherepts_golden(N_POINTS)
+    nhelm = rhelm.copy()
+else:
+    rhelm = spherepts_golden(N_POINTS, hcylind=1)
+    nhelm = rhelm.copy()
+    nhelm[rhelm[:,2]<0, 2] = 0
+    
 npoints = rhelm.shape[0]
-nhelm = rhelm.copy()
-nhelm[rhelm[:,2]<0, 2] = 0
 
 # Compute VSH's
 sss_params = {'origin': np.zeros(3), 'int_order': LIN, 'ext_order': LOUT}
@@ -26,14 +38,19 @@ S = _sss_basis(sss_params, allcoils)
 vsh = np.stack((S[:npoints,:], S[npoints:2*npoints,:], S[2*npoints:]), axis=2) # npoints-by-ncomp-by-3
 
 # Plot
-vmax = np.max(np.linalg.norm(vsh[:,_deg_ord_idx(DEG, ORDER),:], axis=1))
+vmax = np.max(np.linalg.norm(vsh, axis=2))
 
 fig0 = mlab.figure()
-viz_field(rhelm, vsh[:,_deg_ord_idx(DEG, ORDER),:], nhelm, figure=fig0, colormap=CMAP, cmap_range=(-vmax, vmax), show_arrows=True, opacity=0.7, inner_surf=0.3)
+viz_field(rhelm, vsh[:,_deg_ord_idx(DEG_1, ORDER_1),:], nhelm, figure=fig0, colormap=CMAP, cmap_range=(-vmax, vmax), show_arrows=True, opacity=0.7, inner_surf=0.3)
 
 fig1 = mlab.figure()
-viz_field(rhelm, vsh[:,_deg_ord_idx(DEG, ORDER),:], nhelm, figure=fig1, colormap=CMAP, cmap_range=(-vmax, vmax), proj_field=False)
+viz_field(rhelm, vsh[:,_deg_ord_idx(DEG_1, ORDER_1),:], nhelm, figure=fig1, colormap=CMAP, cmap_range=(-vmax, vmax), proj_field=False)
+
+fig2 = mlab.figure()
+viz_field(rhelm, vsh[:,_deg_ord_idx(DEG_2, ORDER_2),:], nhelm, figure=fig2, colormap=CMAP, cmap_range=(-vmax, vmax), proj_field=False)
 
 mlab.sync_camera(fig0, fig1)
+mlab.sync_camera(fig0, fig2)
+
 
 mlab.show()
