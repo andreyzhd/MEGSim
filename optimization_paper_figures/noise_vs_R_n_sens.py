28948670d2440b109b868c8ce657915124ecdac4
andrey.zhdanov@alumni.aalto.fi <Andrey>
2022-03-16 23:49:42 +0200

    Code for plotting noise vs R or n_sens for regular arrays is somewhat ready

--- optimization_paper_figures/noise_vs_R_n_sens.py
+++ optimization_paper_figures/noise_vs_R_n_sens.py
@@ -4,10 +4,18 @@ Generate a plot of noise vs R and number of sensors for regularly-spaced barbute
 
 import numpy as np
 from mayavi import mlab
+import matplotlib.pyplot as plt
 from megsimutils.arrays import BarbuteArraySL, noise_max
 
-N_SENS_RANGE = list(range(120, 241, 10))
-R_RANGE = np.linspace(0.15, 0.25, 10)
+N_SENS_RANGE = np.arange(120, 241, 5)
+R_RANGE = np.linspace(0.15, 0.25, 20)
+
+#N_SENS_RANGE = np.arange(120, 241, 20)
+#R_RANGE = np.linspace(0.15, 0.25, 10)
+
+N_SENS_SUBSAMP = np.arange(4, len(N_SENS_RANGE), 4)
+R_SUBSAMP = np.arange(0, len(R_RANGE), 4)
+
 
 #%% Parameter definitions
 PARAMS = {'n_sens' : 240,
@@ -34,6 +42,22 @@ for i in range(len(N_SENS_RANGE)):
         sens_array = BarbuteArraySL(N_SENS_RANGE[i], PARAMS['l_int'], R_inner=R_RANGE[j], R_outer=PARAMS['R_outer'], n_samp_layers=PARAMS['n_samp_layers'], n_samp_per_layer=PARAMS['n_samp_per_layer'], debug_fldr='/tmp/', **PARAMS['kwargs'])
         noise[i,j] = sens_array.comp_fitness(sens_array.get_init_vector())
 
-x, y = np.meshgrid(N_SENS_RANGE, R_RANGE)
-mlab.surf(noise)
-mlab.show()
+
+#%% Plot
+#x, y = np.meshgrid(N_SENS_RANGE, R_RANGE)
+#mlab.surf(np.log10(noise))
+#mlab.show()
+
+
+plt.figure()
+plt.semilogy(N_SENS_RANGE, noise[:, R_SUBSAMP])
+plt.legend(list(('R = %0.2f' % R) for R in R_RANGE[R_SUBSAMP]))
+plt.xlabel('Number of sensors')
+plt.ylabel('Noise amplification factor')
+
+plt.figure()
+plt.semilogy(R_RANGE, (noise[N_SENS_SUBSAMP, :]).T)
+plt.legend(list(('%i sensors' % n_sens) for n_sens in N_SENS_RANGE[N_SENS_SUBSAMP]))
+plt.xlabel('R, m')
+plt.ylabel('Noise amplification factor')
+plt.show()
