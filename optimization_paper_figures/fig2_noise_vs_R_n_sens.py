ea8e3495d9a5ef3a789df9baab803add5d57455f
andrey.zhdanov@alumni.aalto.fi <Andrey>
2022-03-16 19:46:30 +0200

    WIP

--- optimization_paper_figures/fig2_noise_vs_R_n_sens.py
+++ optimization_paper_figures/fig2_noise_vs_R_n_sens.py
@@ -1,10 +1,13 @@
 """
-Generate a picture of uniformly spaced sensors on a barbute
+Generate a plot of noise vs R and number of sensors for regularly-spaced barbutes
 """
 
 import numpy as np
 from mayavi import mlab
 from megsimutils.arrays import BarbuteArraySL, noise_max
+
+N_SENS_RANGE = list(range(120, 241, 10))
+R_RANGE = np.linspace(0.15, 0.25, 10)
 
 #%% Parameter definitions
 PARAMS = {'n_sens' : 240,
@@ -24,6 +27,13 @@ PARAMS = {'n_sens' : 240,
                       }
           }
 
-sens_array = BarbuteArraySL(PARAMS['n_sens'], PARAMS['l_int'], R_inner=PARAMS['R_inner'], R_outer=PARAMS['R_outer'], n_samp_layers=PARAMS['n_samp_layers'], n_samp_per_layer=PARAMS['n_samp_per_layer'], debug_fldr='/tmp/', **PARAMS['kwargs'])
-sens_array.plot(sens_array.get_init_vector())
+noise = np.zeros((len(N_SENS_RANGE), len(R_RANGE)))
+
+for i in range(len(N_SENS_RANGE)):
+    for j in range(len(R_RANGE)):
+        sens_array = BarbuteArraySL(N_SENS_RANGE[i], PARAMS['l_int'], R_inner=R_RANGE[j], R_outer=PARAMS['R_outer'], n_samp_layers=PARAMS['n_samp_layers'], n_samp_per_layer=PARAMS['n_samp_per_layer'], debug_fldr='/tmp/', **PARAMS['kwargs'])
+        noise[i,j] = sens_array.comp_fitness(sens_array.get_init_vector())
+
+x, y = np.meshgrid(N_SENS_RANGE, R_RANGE)
+mlab.surf(noise)
 mlab.show()
