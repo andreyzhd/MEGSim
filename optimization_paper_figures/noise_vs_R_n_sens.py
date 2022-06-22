1cd1d9704081397d1f4f7fd4a303b1eb02d9bb99
Andrey Zhdanov <andrey.zhdanov@alumni.aalto.fi>
2022-06-22 12:48:13 +0300

    noise-vs-R now computed for L_ext=3

--- optimization_paper_figures/noise_vs_R_n_sens.py
+++ optimization_paper_figures/noise_vs_R_n_sens.py
@@ -7,7 +7,7 @@ from mayavi import mlab
 import matplotlib.pyplot as plt
 from megsimutils.arrays import BarbuteArraySL, noise_max
 
-N_SENS_RANGE = np.arange(120, 241, 5)
+N_SENS_RANGE = np.arange(135, 241, 5)
 R_RANGE = np.linspace(0.15, 0.25, 20)
 
 #N_SENS_RANGE = np.arange(120, 241, 20)
@@ -22,11 +22,11 @@ PARAMS = {'n_sens' : 240,
           'R_inner' : 0.15,
           'R_outer' : None,
           'l_int' : 10,
+          'l_ext': 3,
           'n_samp_layers' : 1,
           'n_samp_per_layer' : 1000,
           'kwargs' : {#'Re' : 0.2,               # Radius for energy-based normalization
                       'height_lower' : 0.15,
-                      'l_ext' : 0,
                       'opm' : False,
                       'origin' : np.array([[0., 0., 0.],]),
                       #'ellip_sc' : np.array([1.2, 1., 1.1])
@@ -39,7 +39,7 @@ noise = np.zeros((len(N_SENS_RANGE), len(R_RANGE)))
 
 for i in range(len(N_SENS_RANGE)):
     for j in range(len(R_RANGE)):
-        sens_array = BarbuteArraySL(N_SENS_RANGE[i], PARAMS['l_int'], R_inner=R_RANGE[j], R_outer=PARAMS['R_outer'], n_samp_layers=PARAMS['n_samp_layers'], n_samp_per_layer=PARAMS['n_samp_per_layer'], debug_fldr='/tmp/', **PARAMS['kwargs'])
+        sens_array = BarbuteArraySL(N_SENS_RANGE[i], PARAMS['l_int'], PARAMS['l_ext'], R_inner=R_RANGE[j], R_outer=PARAMS['R_outer'], n_samp_layers=PARAMS['n_samp_layers'], n_samp_per_layer=PARAMS['n_samp_per_layer'], debug_fldr='/tmp/', **PARAMS['kwargs'])
         noise[i,j] = sens_array.comp_fitness(sens_array.get_init_vector())
 
 
