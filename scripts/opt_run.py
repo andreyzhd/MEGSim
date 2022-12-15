5cfec118e55bdb33c3448e7302afd5130abfed4d
Andrey Zhdanov <andrey.zhdanov@alumni.aalto.fi>
2022-12-15 11:22:38 +0200

    opt_run changed to a thin barbute

--- scripts/opt_run.py
+++ scripts/opt_run.py
@@ -19,11 +19,11 @@ from megsimutils.arrays import BarbuteArraySL, ConstraintPenalty, noise_max
 #%% Parameter definitions
 PARAMS = {'n_sens' : 240,
           'R_inner' : 0.15,
-          'R_outer' : 0.25,
+          'R_outer' : None,
           'l_int' : 10,
           'l_ext' : 3,
-          'n_samp_layers' : 5,
-          'n_samp_per_layer' : 500,
+          'n_samp_layers' : 1,
+          'n_samp_per_layer' : 1000,
           'kwargs' : {#'Re' : 0.2,               # Radius for energy-based normalization
                       'height_lower' : 0.15,
                       'opm' : False,
