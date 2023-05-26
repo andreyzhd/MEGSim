79da645802b03b26ad53dd18438a5ca3c583c4b4
Andrey Zhdanov <andrey.zhdanov@alumni.aalto.fi>
2023-05-27 00:53:03 +0300

    Added head mesh downsampling
    Supposed to speed up computations

--- megsimutils/arrays/BarbuteArrayScalp.py
+++ megsimutils/arrays/BarbuteArrayScalp.py
@@ -1,10 +1,15 @@
 import numpy as np
+import trimesh
+import trimesh.sample
 
 from megsimutils.arrays import BarbuteArraySL
 from megsimutils.viz import _load_anatomy
 
+# Downsample the head mesh to this number of vertices to speed up 
+# computations
+_NVERT_HEAD_HEAD = 700
 
-def _map_barbute_to_scalp_point(point, R_inner, R_outer, head_mesh, d=0.007):
+def _map_barbute_to_scalp_point(point, R_inner, R_outer, head_pts, d=0.007):
     """ Map a single 3D point from a 3D-barbute space to a scalp-array-3D-barbute space
     """
     point = point.copy()
@@ -21,8 +26,8 @@ def _map_barbute_to_scalp_point(point, R_inner, R_outer, head_mesh, d=0.007):
 
     point_norm = point / r
 
-    projs = (head_mesh['rr'] + transl) @ point_norm
-    dists = np.linalg.norm(np.outer(projs, point_norm) - (head_mesh['rr'] + transl), axis=1)
+    projs = (head_pts + transl) @ point_norm
+    dists = np.linalg.norm(np.outer(projs, point_norm) - (head_pts + transl), axis=1)
     dists[projs<=0] = np.max(dists)
 
     R_inner_local = projs[np.argmin(dists)] + d
@@ -36,10 +41,10 @@ def _map_barbute_to_scalp_point(point, R_inner, R_outer, head_mesh, d=0.007):
     return point_new
 
 
-def _map_barbute_to_scalp(pts, R_inner, R_outer, head_mesh, d=0.007):
+def _map_barbute_to_scalp(pts, R_inner, R_outer, head_pts, d=0.007):
     """ Map a set of points 3D point from a 3D-barbute space to a scalp-array-3D-barbute space
     """
-    return np.vstack(list(_map_barbute_to_scalp_point(point, R_inner, R_outer, head_mesh, d=d) for point in pts))
+    return np.vstack(list(_map_barbute_to_scalp_point(point, R_inner, R_outer, head_pts, d=d) for point in pts))
 
 
 class BarbuteArrayScalp(BarbuteArraySL):
@@ -54,12 +59,16 @@ class BarbuteArrayScalp(BarbuteArraySL):
                          n_samp_per_layer=n_samp_per_layer,
                          **kwargs)
         
-        self.__head_mesh = _load_anatomy()['head']
+        head_mesh = _load_anatomy()['head']
+        # Downsample the head mesh to speed up computation
+        mesh = trimesh.Trimesh(head_mesh['rr'], head_mesh['tris'])
+        self.__head_pts = trimesh.sample.sample_surface_even(mesh, _NVERT_HEAD_HEAD)[0]
+
         self.__dist_sens_to_scalp = dist_sens_to_scalp
-        self._sampling_locs_rmags = _map_barbute_to_scalp(self._sampling_locs_rmags, self._R_inner, self._R_outer, self.__head_mesh, d=self.__dist_sens_to_scalp)
+        self._sampling_locs_rmags = _map_barbute_to_scalp(self._sampling_locs_rmags, self._R_inner, self._R_outer, self.__head_pts, d=self.__dist_sens_to_scalp)
 
     def _v2sens_geom(self, v):
         rmags, nmags = super()._v2sens_geom(v)
 
-        return _map_barbute_to_scalp(rmags, self._R_inner, self._R_outer, self.__head_mesh, d=self.__dist_sens_to_scalp), nmags
+        return _map_barbute_to_scalp(rmags, self._R_inner, self._R_outer, self.__head_pts, d=self.__dist_sens_to_scalp), nmags
   
